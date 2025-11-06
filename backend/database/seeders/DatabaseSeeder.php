03e22ca8750e8b84d71cdee2fb2ab2c130e8cf8b
Mario Arnau <mas106@alu.ua.es>
2025-11-06 20:43:33 +0100

    Seeders actualizados para tener la estructura del futbol español senior masculino y femenino (profesional, semi-profesional y amateur).

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         RegionsSeeder::class,
         CategoriesSeeder::class,
         SeasonsSeeder::class,
-        LeaguesDemoSeeder::class,
+        OfficialCompetitionsSeeder::class,
         SuperAdminSeeder::class,
     ]);
     }
