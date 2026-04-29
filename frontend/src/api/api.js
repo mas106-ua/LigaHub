0e924898a7c8260f0784548c1efc0b92bb7bba4f
Mario Arnau <mas106@alu.ua.es>
2026-04-29 13:47:04 +0200

    feat(frontend): añade ui para invitar a ligas privadas por correo

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE,
   withCredentials: true,
+  withXSRFToken: true,
   xsrfCookieName: "XSRF-TOKEN",   
   xsrfHeaderName: "X-XSRF-TOKEN", 
 });
